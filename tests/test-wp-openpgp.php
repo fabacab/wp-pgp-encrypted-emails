3d9abee094505e1811dc47b405714409e61fc342
Meitar Moscovitz <meitarm@gmail.com>
2016-02-21 12:20:41 -0700

    Add test harness.

---
+++ tests/test-wp-openpgp.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Test cases for OpenPGP handling in WordPress.
+ */
+
+/**
+ * WP_OpenPGP test cases.
+ */
+class WP_OpenPGP_TestCase extends Plugin_UnitTestCase {
+
+    public function testGetKey () {
+        $this->markTestIncomplete();
+    }
+
+    public function testEncrypt () {
+        $this->markTestIncomplete();
+    }
+
+    public function testFilterOpenpgpKey () {
+        $this->markTestIncomplete();
+    }
+
+    public function testFilterOpenpgpEncrypt () {
+        $this->markTestIncomplete();
+    }
+
+}
